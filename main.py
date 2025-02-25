a9b47424c49358cd4f3fbe1ed99e20737038f555
YuraIvasiutyn <yuraivasyutin221@gmail.com>
2025-02-25 15:05:30 +0100

    dz7

---
+++ main.py
@@ -0,0 +1,103 @@
+from controler import AddressBook, Record
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Contact not found."
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Incomplete command. Please try again."
+    return inner
+
+def parse_input(user_input: str):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+@input_error
+def add_contact(args, book: AddressBook):
+    name, phone, *_ = args
+    record = book.find(name)
+    message = "Contact updated."
+    if record is None:
+        record = Record(name)
+        book.add_record(record)
+        message = "Contact added."
+    if phone:
+        record.add_phone(phone)
+    return message
+
+@input_error
+def change_contact(args, book: AddressBook):
+    name, old_phone, new_phone, *_ = args
+    record = book.find(name)
+    if record is None:
+        return "Contact not found."
+    record.edit_phone(old_phone, new_phone)
+    return "Contact updated."
+
+@input_error
+def find_phone(args, book: AddressBook):
+    name, *_ = args
+    record = book.find(name)
+    if record is None:
+        return "Contact not found."
+    
+    phones = "; ".join(p.value for p in record.phones)
+    return f"Phone number(s) for {name}: {phones}"
+
+@input_error
+def add_birthday(args, book: AddressBook):
+    name, birthday, *_ = args
+    record = book.find(name)
+    if record is None:
+        return "Contact not found."
+    record.add_birthday(birthday)
+    return "Birthday added."
+
+@input_error
+def show_birthday(args, book: AddressBook):
+    name, *_ = args
+    record = book.find(name)
+    if record is None:
+        return "Contact not found."
+    return f"{record.name.value}'s birthday is on {record.birthday.value}" if record.birthday else "Birthday not set."
+
+
+def main():
+    book = AddressBook()
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ").strip().lower()
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, book))
+        elif command == "change":
+            print(change_contact(args, book))
+        elif command == "phone":
+            print(find_phone(args, book))
+        elif command == "all":
+            print(book)
+        elif command == "add-birthday":
+            print(add_birthday(args, book))
+        elif command == "show-birthday":
+            print(show_birthday(args, book))
+        elif command == "birthdays":
+            print(book.get_upcoming_birthdays())
+        else:
+            print("Invalid command")
+
+
+if __name__ == "__main__":
+    main()
+
